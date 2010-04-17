f565d4d5dda694f4f7597ff1041d30eb2bbcf2a0
Eric Perko <wisesage5001@gmail.com>
2010-04-16 22:45:22 -0400

    Removed a duplicate migration

--- db/migrate/20091023203350_add_wiki_pages_categories.rb
+++
@@ -1,9 +0,0 @@
-class AddWikiPagesCategories < ActiveRecord::Migration
-  def self.up
-    add_column :wiki_pages, :categories, :string
-  end
-
-  def self.down
-    remove_column :wiki_pages, :categories
-  end
-end
